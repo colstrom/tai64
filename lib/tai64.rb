5fce36e2c5dc4607031679225405043a9517a40a
Chris Olstrom <chris@olstrom.com>
2016-07-20 17:51:42 -0700

    Align else/end with if

--- lib/tai64.rb
+++ lib/tai64.rb
@@ -62,10 +62,10 @@ module Tai64
       s = '%016x%08x'
       sec = tai_second
       ts = if sec >= 0
-        sec + EPOCH
-      else
-        EPOCH - sec
-      end
+             sec + EPOCH
+           else
+             EPOCH - sec
+           end
       Label.new s % [ts, tai_nanosecond]
     end
 
